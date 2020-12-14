93b58e031544abaede02efeef22c2009b3bbc72e
dominic-dc <dominicadrian.delacruz17@gmail.com>
2020-12-15 00:07:50 +0800

    Added week 1 assessment

---
+++ Manipulating Images/images/image.py
@@ -0,0 +1,23 @@
+from PIL import Image
+import os
+
+parent_dir = os.getcwd()
+print("Current working directory:", os.getcwd())
+directory = os.path.join("opt", "icons")
+
+
+files = os.listdir("images")
+
+
+def operate(files):
+    for file in files:
+        try:
+            name = os.path.splitext(file)[0]
+            outfile = os.path.join(parent_dir, directory, name)
+            im = Image.open(os.path.join(parent_dir, "images", file))
+            im.rotate(90).resize((128,128)).convert("RGB").save(outfile + ".jpeg")
+        except OSError:
+            continue
+
+operate(files)
+
